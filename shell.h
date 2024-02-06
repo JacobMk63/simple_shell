eea64f9331ccad512939046e6ad95223d883285d
jacobmk <jacobmengistu63@gmail.com>
2024-02-06 12:54:06 -0800

    fixing

--- shell.h
+++ shell.h
@@ -11,6 +11,7 @@
 #include <sys/stat.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <shell.h>
 
 /* for reading /writes of the  buffers */
 #define READ_BUF_SIZE 1024
@@ -153,7 +154,7 @@ char *_strncat(char *, char *, int);
 char **strtow2(char *, char);
 char **strtow(char *, char *);
 
-/* atoi.c */
+/* _atoi.c */
 int interactive(info_t *);
 int is_delim(char, char *);
 int _isalpha(int);
